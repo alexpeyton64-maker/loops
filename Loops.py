3f5cf3a4295e21bb9b58c0c199b8ba12d233e11c
peytona063 <peytona063@gmail.com>
2026-08-28 16:54:14 -0400

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,10 @@
+x = 1
+while x <= 100:
+   print(x)
+   x += 1
+name = ""
+while not name or name.isspace():
+   name = input("Please enter your name")
+print("Hello, %s!" % name)
+for number in range(1,101):
+    print(number)
